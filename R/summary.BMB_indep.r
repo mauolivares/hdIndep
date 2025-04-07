2449d0a94265e6af2bc159f6367fde9a5409040d
Mauricio Olivares <lehmann@eduroam-int-dhcp-97-152-152.ucl.ac.uk>
2025-04-07 13:50:54 +0200

    Update summary method and round output

--- R/summary.BMB_indep.r
+++ R/summary.BMB_indep.r
@@ -13,41 +13,34 @@
 #' @author Daniel Wilhelm
 #' @export
 
-summary.BMB_indep<-function(object, ..., digits=max(3, getOption("digits") - 3)){
-
+summary.BMB_indep <- function(object, ..., digits = 4) {
   cat("\n")
   cat("**************************************************\n")
   cat("**   Powerful Block-Multiplier Bootstrap Test   **\n")
   cat("**   of Independence in High Dimensions         **\n")
   cat("**************************************************\n")
   cat("* -----------------------------------------------*\n")
-  cat("     H0: X independent of Y1, Y2, ..., Yp")
-  cat("\n")
-  cat("* -----------------------------------------------*\n")
-  cat("\n")
-  cat(paste("Number of Hypotheses: ", object$p, sep = ""))
-  cat("\n")
-  cat(paste("Sample Size: ", object$n, sep = ""))
-  cat("\n")
-  cat(paste("Value of the test statistics ", round(object$T_obs, 4), sep = ""))
-  cat("\n")
-  cat(paste("Bootstrap Critical Value: ", round(object$cv,4), sep = ""))
-  cat("\n")
-  if (object$type == "bmb") {
-    cat("Critical value computed from the conditional distribution of the unstudentized bootstrap test statistic.")
-  } else if (object$type == "bmb1") {
-    cat("Critical value computed from the conditional distribution of")
-    cat("the bootstrap statistic studentized by the square root of the.")
-    cat("big block's second moment divided by the block size.")
+  cat("     H0: X independent of Y1, Y2, ..., Yp\n")
+  cat("* -----------------------------------------------*\n\n")
 
+  cat("Number of Hypotheses:       ", object$p, "\n")
+  cat("Sample Size:                ", object$n, "\n")
+  cat("Value of Test Statistic:    ", formatC(object$T_obs, digits = digits, format = "f"), "\n")
+  cat("Bootstrap Critical Value:   ", formatC(object$cv, digits = digits, format = "f"), "\n")
+
+  if (object$type == "bmb") {
+    cat("Critical value computed from the conditional distribution of the unstudentized bootstrap test statistic.\n")
+  } else if (object$type == "bmb1") {
+    cat("Critical value computed from the conditional distribution of the bootstrap statistic\n")
+    cat("studentized by the square root of the big block's second moment divided by the block size.\n")
   } else if (object$type == "bmb2") {
-    cat("Critical value computed from the conditional distribution of the bootstrap statistic studentized by standardized demeaned big blocks.")
+    cat("Critical value computed from the conditional distribution of the bootstrap statistic\n")
+    cat("studentized by standardized demeaned big blocks.\n")
   }
-  cat("\n")
-  cat(paste("Optimal Block Size: ", object$block_size, sep = ""))
-  cat("\n")
-  cat(paste("Number of Bootstrap Samples: ", object$B, sep = ""))
-  cat("\n")
-  cat(paste("Decision: ", object$decision, " at significance level ", object$alpha, sep = ""))
-  cat("\n")
+
+  cat("Optimal Block Size:         ", object$block_size, "\n")
+  cat("Number of Bootstrap Samples:", object$B, "\n")
+  cat("Decision:                   ", object$decision, " at significance level ", object$alpha, "\n")
+
+  invisible(object)
 }
